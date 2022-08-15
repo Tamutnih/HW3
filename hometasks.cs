45f5253f8ffd592b6a21950b64f6811b03004833
tatyana mutnykh <renu07@list.ru>
2022-08-15 20:32:56 +0500

    Решение

--- hometasks.cs
+++ hometasks.cs
@@ -42,30 +42,56 @@
 
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 
-Console.WriteLine("Введите координату: x1 ");
-int x1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите координату: y1 ");
-int y1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите координату: z1 ");
-int z1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите координату: x2 ");
-int x2 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите координату: y2 ");
-int y2 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите координату: z2 ");
-int z2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите координату: x1 ");
+// int x1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите координату: y1 ");
+// int y1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите координату: z1 ");
+// int z1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите координату: x2 ");
+// int x2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите координату: y2 ");
+// int y2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите координату: z2 ");
+// int z2 = Convert.ToInt32(Console.ReadLine());
 
-double Decision(double x1, double x2, 
-                double y1, double y2, 
-                double z1, double z2){
-  return Math.Sqrt(Math.Pow((x2-x1), 2) + 
-                   Math.Pow((y2-y1), 2) + 
-                   Math.Pow((z2-z1), 2));
+// double Decision(double x1, double x2, 
+//                 double y1, double y2, 
+//                 double z1, double z2){
+//   return Math.Sqrt(Math.Pow((x2-x1), 2) + 
+//                    Math.Pow((y2-y1), 2) + 
+//                    Math.Pow((z2-z1), 2));
+// }
+
+// double segmentLength =  Math.Round (Decision(x1, x2, y1, y2, z1, z2), 2 );
+
+// Console.WriteLine($"Длина отрезка  {segmentLength}");
+
+//Задача 23: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+
+Console.Clear();
+Console.Write("Введите число N: ");
+int N=Convert.ToInt32(Console.ReadLine());
+int ind = 1;
+if (N>0)
+{
+    while (ind<=N)
+    {
+        Console.Write(Math.Pow(ind,3) + ", ");
+        ind=ind+1;
+    }
 }
-
-double segmentLength =  Math.Round (Decision(x1, x2, y1, y2, z1, z2), 2 );
-
-Console.WriteLine($"Длина отрезка  {segmentLength}");
+else
+{
+    while (ind>=N)
+    {
+        Console.Write(Math.Pow(ind,3) + ", ");
+        ind=ind-1;
+    }
+}
 
  
 
